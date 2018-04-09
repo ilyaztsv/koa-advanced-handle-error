3d146aefefe3c6b05f748fd99f2d2eb80956ef43
Ilya Zaytsev <i.zaytsev@office.ngs.ru>
2018-04-09 22:00:35 +0700

    refactor: remove flow

--- build.js
+++ build.js
@@ -1,19 +1,15 @@
 const fs = require('fs-path');
 
-require('@babel/core').transformFile(
-  'index.js',
-  {},
-  (err, result) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-
-    try {
-      fs.writeFileSync('lib/index.js', result.code);
-    } catch (err) {
-      console.error(err);
-      process.exit(1);
-    }
+require('@babel/core').transformFile('index.js', {}, (err, result) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
   }
-);
+
+  try {
+    fs.writeFileSync('lib/index.js', result.code);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
