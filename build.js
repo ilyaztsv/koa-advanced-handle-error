c75a345a2746e923ac4131b18069ba987f52278b
Ilya Zaytsev <i.zaytsev@office.ngs.ru>
2018-04-11 21:31:22 +0700

    refactor (#8): move index.js to middleware folder

--- build.js
+++ build.js
@@ -1,15 +1,19 @@
 const fs = require('fs-path');
 
-require('@babel/core').transformFile('index.js', {}, (err, result) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
+require('@babel/core').transformFile(
+  'middleware/index.js',
+  {},
+  (err, result) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
 
-  try {
-    fs.writeFileSync('lib/index.js', result.code);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
+    try {
+      fs.writeFileSync('lib/index.js', result.code);
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
   }
-});
+);
